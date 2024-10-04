42bf9b56d5652de4e52301d8f263a8fc3bf88424
senky <jakubsenko@gmail.com>
2024-10-04 14:53:40 +0200

    add typesafe-routes package
    That was my biggest issue with Astro so far. I was missing the typesafe routes. I know it needs to generate arbitrary file to make it work, but with this kind of website you cannot afford to have a dead links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import typesafeRoutes from 'astro-typesafe-routes';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://visitstaralubovna.com',
-	integrations: [mdx(), sitemap()],
-	i18n: {
-		defaultLocale: 'sk',
-		locales: ['en', 'pl', 'sk'],
-	}
+    site: 'https://visitstaralubovna.com',
+    integrations: [mdx(), sitemap(), typesafeRoutes()],
+    i18n: {
+        defaultLocale: 'sk',
+        locales: ['en', 'pl', 'sk'],
+    }
 });
