3ab673506516de4527614e5f7ede0368f71aeac2
senky <jakubsenko@gmail.com>
2024-10-15 14:10:22 +0200

    add tailwind and typesafe routes
    I am so looking forward to use tailwind finally after all those years. I am very skeptical about it even still, but there is no other way to get my arguments to the point other than actually trying it.
    
    And typesafe routes? My heart almost stopped when I realized Astro doesn't do any route checking at all, they are all pure `a` tags. Wtf?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 import typesafeRoutes from 'astro-typesafe-routes';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://visitstaralubovna.com',
-    integrations: [mdx(), sitemap(), typesafeRoutes()],
+    integrations: [mdx(), sitemap(), tailwind(), typesafeRoutes()],
     i18n: {
         defaultLocale: 'sk',
         locales: ['en', 'pl', 'sk'],
